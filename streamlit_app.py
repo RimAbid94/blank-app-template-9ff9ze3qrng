a70446730868199edb1f41556083c9b730532694
RimAbid94 <rim.abid@wearenium.com>
2024-07-03 20:19:28 +0100

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,6 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    cylinders = pn.widgets.IntSlider(name='Cylinders', start=4, end=8, step=2)
+cylinders
 )
